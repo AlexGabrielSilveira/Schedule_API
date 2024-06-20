cf6f49a0b7521e472e234939466b37d8ed07f399
AlexGabrielSilveira <alexgabrielyou@gmail.com>
2024-06-20 09:51:53 -0300

    Estudo sobre entity framework #1

---
+++ Context/ScheduleContext.cs
@@ -0,0 +1,15 @@
+﻿using DIO.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DIO.Context
+{
+
+    public class ScheduleContext : DbContext
+    {
+        public ScheduleContext(DbContextOptions<ScheduleContext> options) : base(options) {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace DIO.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public string  Phone { get; set; }
+        public bool IsActive{ get; set; }
+    }
+}
